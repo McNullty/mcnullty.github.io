f0e009b13b2e7515ae13a3e6eafccc4fe01d2d7c
Mladen Čikara <mladen.cikara@gmail.com>
2019-10-27 08:54:25 +0100

    Adding service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+const fontRequest = new Request('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
+const cacheName = 'elm-stopwatch';
+const filesToCache = [
+    '/',
+    '/index.html',
+    fontRequest,
+    '/elm.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
